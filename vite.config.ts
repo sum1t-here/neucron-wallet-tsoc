a9979ffe9344a145a9d38a66eb108792deb0259f
sumit <mazumdarsumit3@gmail.com>
2024-07-12 17:12:44 +0530

    add: bounty

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import babel from "@rollup/plugin-babel";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**", // only transpile our source code
+      extensions: [".js", ".ts", ".jsx", ".tsx", ".svelte"],
+    }),
+  ],
 });

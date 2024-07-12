37c5fc6f219abf723c1a01e8059b81e90ca2833e
sumit <mazumdarsumit3@gmail.com>
2024-07-12 15:33:10 +0530

    add: cloud wallet working fine

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -4,14 +4,18 @@ export const nodes = [
 	() => import('./nodes/0'),
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
-	() => import('./nodes/3')
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
 		"/": [2],
-		"/profiles": [~3]
+		"/cloud-wallet": [~3],
+		"/profiles": [~4],
+		"/raw-wallet": [5]
 	};
 
 export const hooks = {
---
+++ src/routes/raw-wallet/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import {PrivateKey} from '@bsv/sdk'
+
+    let WIF:string
+    let address:string
+
+    function createKeys(){
+        const privKey = PrivateKey.fromRandom()
+        WIF = privKey.toWif()
+
+        console.log("Private key WIF format is ", WIF)
+        address = privKey.toAddress()
+        console.log("Your bitcoin address is ", address)
+    }
+</script>
+<section class="flex flex-col gap-5 items-center">
+    <div>
+        <button on:click={createKeys} class="border rounded-lg bg-black text-white p-3">
+            Generate new wallet keys
+        </button>
+    </div>
+    <div class="flex flex-col gap-5">
+        <p>
+            Private key WIF format is <span class=" text-green-500">{WIF}</span>
+        </p>
+        <p>
+            Your Bitcoin Address is <span class=" text-green-500">{address}</span>
+        </p>
+    </div>  
+</section>

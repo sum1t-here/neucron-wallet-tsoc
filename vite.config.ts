eefd0cec5727035c7d7fad4ba55dbc90e3555ee8
sumit <mazumdarsumit3@gmail.com>
2024-07-12 17:18:35 +0530

    removed some packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import babel from "@rollup/plugin-babel";
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    babel({
-      babelHelpers: "bundled",
-      exclude: "node_modules/**", // only transpile our source code
-      extensions: [".js", ".ts", ".jsx", ".tsx", ".svelte"],
-    }),
-  ],
+  plugins: [sveltekit()],
 });
